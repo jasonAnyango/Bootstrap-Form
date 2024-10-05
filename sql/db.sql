5d5e678dc2b87be9c77657e6c6e3991c607b1e9b
jasonAnyango <jason.anyango@strathmore.edu>
2024-10-05 10:35:48 +0300

    Changed the sql code to reflect form inputs. Created the database

--- sql/db.sql
+++ sql/db.sql
@@ -1,17 +1,14 @@
 -- Database creation
-DROP DATABASE IF EXISTS users;
-CREATE DATABASE users;
+DROP DATABASE IF EXISTS assignment2;
+CREATE DATABASE assignment2;
 
-USE users;
+USE assignment2;
 -- Table: users
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     userID INT NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
     username varchar(255) NOT NULL,
-    fname varchar(255) NOT NULL,
-    surname varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    fullname varchar(255) NOT NULL,
     PRIMARY KEY (userID)
 );
-
