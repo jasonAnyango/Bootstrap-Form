fa32d44c4929399090116dce0670205b7c685db1
jasonAnyango <jason.anyango@strathmore.edu>
2024-10-05 10:50:31 +0300

    Redid the signup function in processForm.php

--- database/processForm.php
+++ database/processForm.php
@@ -1,56 +1,36 @@
 <?php
 
-require_once "dbConnection.php";
-
-function validatePassword($password, $confirmPassword)
+class processForm
 {
-    if($password == $confirmPassword)
+    public function signup($conn)
     {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-echo "Start";
-if(isset($_POST['email'], $_POST['username'], $_POST['fname'], $_POST['surname'], $_POST['password'], $_POST['confirmPassword']))
-{
-    // Assign variables
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $fname = $_POST['fname'];
-    $surname = $_POST['surname'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['confirmPassword'];
-
-    // Validate Password
-    if(!validatePassword($password, $confirmPassword))
-    {
-        echo "Passwords do not match";
-        die;
-    }
-    else
-    {
-        try
+        if(isset($_POST['signup']))
         {
-        // Prepare the query
-        $insertQuery = "INSERT INTO users (email, username, fname, surname, password) VALUES :email, :username, :fname, :surname, :password)";
-        //  Create prepared statement
-        $stmt = $connection->prepare($insertQuery);
-        // Bind parameters
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':fname', $fname);
-        $stmt->bindParam(':surname', $surname);
-        $stmt->bindParam(':password', $password);
-        $stmt->execute();
+            // Set Variables
+            $fullname = $_POST['fullname'];
+            $email = $_POST['email'];
+            $username = $_POST['username'];
 
-        echo "Record Inserted Successfully 👌 <br>"; 
-        }
-        catch(PDOException $pdoError)
-        {
-            echo "Record Insertion Failed 😭  <br>" . $pdoError->getMessage();
+            // Make arrays
+            $columns = ['fullname', 'email', 'username'];
+            $values = [$fullname, $email, $username];
+
+            // Combine arrays
+            $data = array_combine($columns, $values);
+
+            // Insert into database
+            $insert  = $conn->insert('users', $data);
+
+            // Check if the insert is valid
+            if($insert === true)
+            {
+                header("Location: signUp.php");
+            }
+            else
+            {
+                die($insert);
+            }
         }
     }
+
 }
