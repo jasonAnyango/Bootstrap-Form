7e1cae84ba50172ba6e55b41c535e29bedf1b2c4
jasonAnyango <jason.anyango@strathmore.edu>
2024-10-05 20:46:38 +0300

    Changed the form input variables to be escaped

--- database/processForm.php
+++ database/processForm.php
@@ -7,9 +7,10 @@ class processForm
         if(isset($_POST['signup']))
         {
             // Set Variables
-            $fullname = $_POST['fullname'];
-            $email = $_POST['email'];
-            $username = $_POST['username'];
+            $fullname = $conn->escapeValues(ucwords(strtolower($_POST['fullname'])));
+            $email = $conn->escapeValues(strtolower($_POST['email']));
+            $username = $conn->escapeValues(strtolower($_POST['username']));
+            
 
             // Input Validation
             // Initialize errors array
